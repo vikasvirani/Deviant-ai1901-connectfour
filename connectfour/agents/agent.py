e3e81a28df1d41a2611790a796f3c25e7463d083
vikasvirani <virani.vikasr@gmail.com>
2019-04-19 17:21:44 +1000

    RollBack

--- connectfour/agents/agent.py
+++ connectfour/agents/agent.py
@@ -8,9 +8,7 @@ class Agent:
         Args:
             board: An instance of `Board` that is the current state of the board.
 
-        Returns:
-            A tuple of two integers, (row, col)
-        """
+        
         raise NotImplementedError
 
     def __repr__(self):
