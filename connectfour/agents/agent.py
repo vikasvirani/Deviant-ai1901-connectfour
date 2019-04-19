11888786f4ff86177e19f1cd969f2f38c09c5b47
vikasvirani <virani.vikasr@gmail.com>
2019-04-19 17:32:31 +1000

    Revert "Update agent.py"
    This reverts commit 37e67ca19752e58f2fce6e46a0543953af7d2102.

--- connectfour/agents/agent.py
+++ connectfour/agents/agent.py
@@ -8,7 +8,9 @@ class Agent:
         Args:
             board: An instance of `Board` that is the current state of the board.
 
-        
+        Returns:
+            A tuple of two integers, (row, col)
+        """
         raise NotImplementedError
 
     def __repr__(self):
